26ae05644f790cbfa797999254020a03ddecac49
abulu <4133020500@163.com>
2017-12-12 20:58:39 +0800

    v1
    first version

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RunNo_MVC.Models;
+
+namespace RunNo_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //int id
+        public IActionResult Index()
+        {
+            // ViewData["Number"] =    
+
+            //     return View();
+
+            // User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299
+            //var ip = Request.Headers["X-Forwarded-For"].FirstOrDefault();
+            string ip = Request.Headers["User-Agent"].FirstOrDefault();
+
+            string iKey = NumberObject.GenerateMD5(ip);
+         
+            Product myProduct = new Product
+            {
+                GUID = iKey,
+                Name = NumberObject.PickNumber(iKey)
+            };
+
+
+            return View(myProduct);
+
+        }
+
+
+        public IActionResult About(int Id)
+        {
+            NumberObject.SetLimitNo(Id.ToString());
+          
+             Product myProduct = new Product
+            {
+                GUID = "SettingNo",
+                Name =Id.ToString()
+            };
+       
+            return View(myProduct);
+        }
+
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Model/NumberHelper.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Text;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+
+namespace RunNo_MVC.Models
+{
+
+    public class Product
+    {
+        public string GUID { get; set; }
+        public string Name { get; set; }
+    }
+    public static class UsedList
+    {
+
+        public static List<Product> UsedProductList = new List<Product>();
+
+
+        public static void ClearAllUsedList()
+        {
+            UsedProductList.Clear();
+        }
+    }
+    public static class NumberObject
+    {
+
+        public static int LimitNo = 0;
+        public static int CountIndex = 0;
+        public static string PickNumber(string identityUser)
+        {
+            Random random = new Random();
+
+            string newnumber = "";
+
+            if (UsedList.UsedProductList.Exists(n => n.GUID == identityUser))
+            {
+                newnumber = UsedList.UsedProductList.Find(n => n.GUID == identityUser).Name;
+            }
+            else
+            {
+                newnumber = NextCountNumber();
+                UsedList.UsedProductList.Add(new Product() { GUID = identityUser, Name = newnumber });
+            }
+
+            return newnumber;
+        }
+
+        public static void SetLimitNo(string newLimitNo)
+        {
+            UsedList.ClearAllUsedList();
+            CountIndex =0;
+            Int32.TryParse(newLimitNo, out LimitNo);
+        }
+
+
+        private static string NextCountNumber()
+        {
+            CountIndex++;
+
+            if (CountIndex > LimitNo)
+                return "号已取完！请等待下一局！";
+
+            return CountIndex.ToString();
+        }
+
+        /// <summary>
+        /// method to generate a MD5 hash of a string
+        /// </summary>
+        /// <param name="strToHash">string to hash</param>
+        /// <returns>hashed string</returns>
+        public static string GenerateMD5(string str)
+        {
+            //  MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            //new 
+
+            MD5 md5 = MD5.Create();
+
+            byte[] byteArray = Encoding.ASCII.GetBytes(str);
+            byteArray = md5.ComputeHash(byteArray);
+            string hashedValue = "";
+            foreach (byte b in byteArray)
+            {
+                hashedValue += b.ToString("x2");
+            }
+            return hashedValue;
+        }
+
+
+    }
+}
